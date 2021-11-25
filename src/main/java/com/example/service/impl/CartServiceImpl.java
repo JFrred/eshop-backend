75c0476f185bc5f6bb81ce2489abed239d0e9f7a
Fryderyk <fryderyk32j@gmail.com>
2021-11-25 17:02:18 +0100

    refactor: delete unused import

--- src/main/java/com/example/service/impl/CartServiceImpl.java
+++ src/main/java/com/example/service/impl/CartServiceImpl.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.management.InvalidAttributeValueException;
 import javax.transaction.Transactional;
 
 @Slf4j
